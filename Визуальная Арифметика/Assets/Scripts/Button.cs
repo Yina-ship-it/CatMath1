904085070cff86bbf908bcd1a308fe3ba548ed3d
Yina-ship-it <84266709+Yina-ship-it@users.noreply.github.com>
2021-05-26 18:36:59 +0500

    Revert "Game1"
    This reverts commit 335a87cec4be938dada7ab6356d06cd30c986476.

--- Визуальная Арифметика/Assets/Scripts/Button.cs
+++ Визуальная Арифметика/Assets/Scripts/Button.cs
@@ -80,7 +80,7 @@ public class Button : MonoBehaviour
                 DataHolder.k = 1;
                 break;
             case 3:
-                DataHolder.N = 5;
+                DataHolder.N = 6;
                 DataHolder.Max = 2000;
                 DataHolder.NumberSigns = 4;
                 DataHolder.k = 0.1;
