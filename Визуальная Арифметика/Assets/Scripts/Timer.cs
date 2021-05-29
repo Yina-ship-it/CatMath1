3c3279c5afdcd857674b82c1481858198fbb12d5
Yina-ship-it <84266709+Yina-ship-it@users.noreply.github.com>
2021-05-29 12:14:54 +0500

    линия времени
    дима соси бибу

---
+++ Визуальная Арифметика/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{
+    public float time;
+    private float dtime;
+    void Start()
+    {
+        float width = GameObject.Find("Canvas").GetComponent<RectTransform>().rect.width;
+        GetComponent<RectTransform>().sizeDelta = new Vector2(width, 10f);
+    }
+
+    void Update()
+    {
+        dtime = Time.deltaTime / time;
+        GetComponent<Image>().fillAmount -= dtime;
+        GetComponent<Image>().color += new Color(dtime / 2, -dtime / 2, -dtime / 2);
+        if (GetComponent<Image>().fillAmount == 0f)
+            GameObject.Find("EventSystem").GetComponent<Game1Ctrl>().lose = true;
+    }
+}
